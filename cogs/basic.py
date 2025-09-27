99f1c22c272a3fbd0e0918db6e7b80198e7ecc28
Vivian Leigh <35174852+Vivi4n@users.noreply.github.com>
2025-09-27 21:09:43 +0300

    Add lurk and hug commands to the basic cog

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,5 +1,6 @@
 # cogs/basic.py
 from twitchio.ext import commands
+import random # < -- Adding this for later, maybe to be used for !8ball or so?
 
 @commands.command(name="hello")
 async def hello(ctx: commands.Context):
@@ -9,4 +10,23 @@ async def hello(ctx: commands.Context):
 async def ping(ctx: commands.Context):
     await ctx.send("Pong 🏓")
 
-commands_list = [hello, ping]
+@commands.command(name="lurk")
+async def lurk(ctx: commands.Context):
+    await ctx.send(f"@{ctx.author.name} is lurking in the shadows...")
+
+@commands.command(name="hug") # Just a hug command :3
+async def hug(ctx: commands.Context, *, user: str = None):
+    hug_emotes = ["theter52DrobenUbu", "theter52ZoeUwU", "theter52LuxUwU", "<3", "theter52SipSip"]
+    
+    target_user = ""
+    if user:
+        target_user = user.lstrip('@') # Removes the @ if someone pings the user with it.
+
+    if not target_user:
+        message = f"@{ctx.author.name} gets a big, warm hug! {random.choice(hug_emotes)}"
+    else:
+        message = f"@{ctx.author.name} gives @{target_user} a super cozy hug! {random.choice(hug_emotes)}"
+        
+    await ctx.send(message)
+
+commands_list = [hello, ping, lurk, hug]
