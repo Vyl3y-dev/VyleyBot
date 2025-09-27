3a33c4b0e97e49b01ee7dab55968c397b5650891
Vivian Leigh <35174852+Vivi4n@users.noreply.github.com>
2025-09-27 22:22:22 +0300

    Added !8ball

--- cogs/basic.py
+++ cogs/basic.py
@@ -29,4 +29,38 @@ async def hug(ctx: commands.Context, *, user: str = None):
         
     await ctx.send(message)
 
-commands_list = [hello, ping, lurk, hug]
+@commands.command(name="8ball")
+async def eight_ball(ctx: commands.commanads.Context, *, question: str = None):
+    if question is None:
+        await ctx.send(f"@{ctx.author.name}, you need to ask a question! Usage: !8ball <question>")
+        return
+
+    responses = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes – definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful.",
+        "Shiiiet idk mane I'm high asf rn ask me later."
+    ]
+    
+    answer = random.choice(responses)
+    await ctx.send(f"🎱 Question: '{question}'\n\n🎱 Magic 8-Ball says: {answer}")
+
+commands_list = [hello, ping, lurk, hug, eight_ball]
+
