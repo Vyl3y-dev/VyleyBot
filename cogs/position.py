70b98ede81f83367e9a4e8dd8a17fd4b55453237
Vivian Leigh <35174852+Vivi4n@users.noreply.github.com>
2025-10-23 01:21:03 +0300

    Make positions work in memory
    With calling upon ctx.author.name as heldPosition is still "" cause we never assigned it, it's always assigned by ctx.author.name

--- cogs/position.py
+++ cogs/position.py
@@ -1,54 +1,31 @@
-import os
-import json
-from typing import Dict
 from twitchio.ext import commands
-
-DATA_PATH = "data/position.json"
-
-def load_position() -> Dict:
-    if not os.path.exists(DATA_PATH):
-        return {}
-    try:
-        with open(DATA_PATH, "r", encoding="utf-8") as f:
-            content = f.read()
-            if not content:
-                return {}
-            return json.loads(content)
-    except (json.JSONDecodeError, FileNotFoundError):
-        return {}
-    
-def save_position(position: Dict):
-    os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)
-    with open(DATA_PATH, "w", encoding="utf-8") as f:
-        json.dump(position, f, indent=2, ensure_ascii=False)
 
 class Position(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+        # Changed to store in memory instead of file, as we want it to reset every time bot is started.
+        self.positions = {}
+
     @commands.command(name="first")
     async def first(self, ctx: commands.Context):
-        positionHolder = load_position()
-        heldPosition = positionHolder.get("calledFirst", "")
+        held_position = self.positions.get("first")
         
-        if heldPosition == "":
-            positionHolder["calledFirst"] = ctx.author.name
-            save_position(positionHolder)
-            await ctx.send(f"{heldPosition} is First!")
-        else:
-            await ctx.send(f"{heldPosition} was First!")
+        if not held_position:
+            self.positions["first"] = ctx.author.name
+            await ctx.send(f"{ctx.author.name} is first! <3")
+            else:
+                await ctx.send(f"{held_position} was already first!")
 
     @commands.command(name="second")
     async def second(self, ctx: commands.Context):
-        positionHolder = load_position()
-        heldPosition = positionHolder.get("calledSeccond", "")
-        
-        if heldPosition == "":
-            positionHolder["calledSecond"] = ctx.author.name
-            save_position(positionHolder)
-            await ctx.send(f"{heldPosition} is Second!")
+        held_position = self.positions.get("second")
+
+        if not held_position:
+            self.positions["second"] = ctx.author.name
+            await ctx.send(f"{ctx.author.name} is second! <3")
         else:
-            await ctx.send(f"{heldPosition} was Second!")
+            await ctx.send(f"{held_position} was already second!")
     
 def prepare(bot: commands.Bot):
     bot.add_cog(Position(bot))
